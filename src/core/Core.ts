699d9319eee7e3f9f9821afe82cac1288af9ba54
John Smith <johnsmithcoder7@gmail.com>
2020-09-30 16:40:23 -0400

    Fix exported stats showing incorrect time played

--- src/core/Core.ts
+++ src/core/Core.ts
@@ -189,6 +189,7 @@ export class Core {
 
                 char.joinTime = first.timestamp;
                 char.secondsOnline = (last.timestamp - first.timestamp) / 1000;
+                this.characters.find(chr => chr.ID === char.characterID).secondsPlayed = char.secondsOnline;
             } else {
                 char.secondsOnline = 0;
             }
